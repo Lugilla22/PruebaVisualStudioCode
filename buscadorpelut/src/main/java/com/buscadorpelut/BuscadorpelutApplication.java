6b5dba4068a061284331ad574639fe2b80980a2e
lugilla22 <lugilla2269@gmail.com>
2025-10-18 15:11:12 +0200

    Creacion de la estructura backend

---
+++ buscadorpelut/src/main/java/com/buscadorpelut/BuscadorpelutApplication.java
@@ -0,0 +1,13 @@
+package com.buscadorpelut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscadorpelutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscadorpelutApplication.class, args);
+	}
+
+}
---
+++ buscadorpelut/src/test/java/com/buscadorpelut/BuscadorpelutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buscadorpelut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscadorpelutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
